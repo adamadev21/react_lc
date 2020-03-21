d9d38734d5a458f8befbb81887b3e23928ecec20
adamadev21 <sawadams09@gmail.com>
2020-03-20 23:31:56 -0500

    changes on ui

--- src/App.js
+++ src/App.js
@@ -9,16 +9,19 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Footer from './Footer';
 import Testimonials from './Testimonials';
 import Testiform from './Testiform';
+import Tutoring from './Tutoring';
 class App extends React.Component {
   render(){
     return (
       <div className="App">
         <Head/>
         <Home/>
+        <Route exact path='/tutoring' component={Tutoring}/>
         <Route exact path='/' component={Register}/>
         <Route exact path='/register' component={Register}/>
         <Route exact path='/login' component={Login}/>
-        <Testimonials/>
+        <Route exact path='/testimonials' component={Testimonials}/>
+
         <Footer/>
       </div>
     )
